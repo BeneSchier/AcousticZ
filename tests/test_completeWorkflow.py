4aee712038474174bc9b59210fd0fab863f665b7
BeneSchier <benedikt.schier@gmail.com>
2023-07-17 19:28:42 +0200

    fixed tests

--- tests/test_completeWorkflow.py
+++ tests/test_completeWorkflow.py
@@ -8,32 +8,14 @@ def test_completeWorkflow():
     shoebox_file_path = current_dir + '/../data/example_meshes/shoebox.obj'
     
     room_file = shoebox_file_path
-    FVect = np.array([125, 250, 500, 1000, 2000, 4000])
-    A = np.array([[0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-                  [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-                  [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-                  [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-                  [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-                  [0.08, 0.09, 0.12, 0.16, 0.22, 0.24]]).T
-    # Reflection coefficients
-    # R = np.sqrt(1 - A)
 
-    # frequency-dependant scattering coefficients
-    D = np.array([[0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.01, 0.05, 0.1, 0.2, 0.3, 0.5],
-                  [0.01, 0.05, 0.1, 0.2, 0.3, 0.5]])
-    room = Room(room_file, numberOfRays=10, absorptionCoefficients=A,
-                scatteringCoefficients=D, FVect=FVect)
+    room = Room(room_file)
     point1 = np.array([2.0, 2.0, 2.0])
-    # point2 = np.array([5.0, 5.0, 1.8])
     point2 = np.array([5.0, 5.0, 1.8])
     room.createReceiver(point2, 0.0875)
     room.createSource(point1)
     
-    room.performRayTracing_vectorized()
+    room.performRayTracing_vectorized(numberOfRays=10)
     assert np.all(room.TFHist >= 0) and not np.any(np.isnan(room.TFHist))
     
     room.generateRoomImpulseResponse()
--- tests/test_room.py
+++ tests/test_room.py
@@ -6,98 +6,21 @@ import os
 def test_volume_calculation():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     shoebox_file_path = current_dir + '/../data/example_meshes/shoebox.obj'
-    FVect = np.array([125, 250, 500, 1000, 2000, 4000])
-    A = np.array([[0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24]]).T
-    # Reflection coefficients
-    R = np.sqrt(1 - A)
-
-    # frequency-dependant scattering coefficients
-    D = np.array([[0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.01, 0.05, 0.1, 0.2, 0.3, 0.5],
-                  [0.01, 0.05, 0.1, 0.2, 0.3, 0.5]])
-    room = Room(shoebox_file_path, numberOfRays=100, absorptionCoefficients=A,
-                scatteringCoefficients=D, FVect=FVect)
-    V = room.getRoomVolume()
+    room = Room(shoebox_file_path)
+    V = room.getBoundaryBoxVolume()
     assert V == 320.0
+
 
 def test_pointIsInsideMesh_True():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     shoebox_file_path = current_dir + '/../data/example_meshes/shoebox.obj'
-    FVect = np.array([125, 250, 500, 1000, 2000, 4000])
-    A = np.array([[0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24]]).T
-    # Reflection coefficients
-    R = np.sqrt(1 - A)
-
-    # frequency-dependant scattering coefficients
-    D = np.array([[0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.01, 0.05, 0.1, 0.2, 0.3, 0.5],
-                  [0.01, 0.05, 0.1, 0.2, 0.3, 0.5]])
-    room = Room(shoebox_file_path, numberOfRays=100, absorptionCoefficients=A,
-                scatteringCoefficients=D, FVect=FVect)
+    room = Room(shoebox_file_path)
     assert room.isPointInsideMesh(np.array([9.999999, 7.999999, 3.999999]))
-    
+
+
 def test_pointIsInsideMesh_False():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     shoebox_file_path = current_dir + '/../data/example_meshes/shoebox.obj'
-    FVect = np.array([125, 250, 500, 1000, 2000, 4000])
-    A = np.array([[0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24]]).T
-    # Reflection coefficients
-    R = np.sqrt(1 - A)
+    room = Room(shoebox_file_path)
 
-    # frequency-dependant scattering coefficients
-    D = np.array([[0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.01, 0.05, 0.1, 0.2, 0.3, 0.5],
-                  [0.01, 0.05, 0.1, 0.2, 0.3, 0.5]])
-    room = Room(shoebox_file_path, numberOfRays=100, absorptionCoefficients=A,
-                scatteringCoefficients=D, FVect=FVect)
-    
     assert not room.isPointInsideMesh(np.array([10.00001, 800001, 4.00001]))
-    
-def test_applyRIR():
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    shoebox_file_path = current_dir + '/../data/example_meshes/shoebox.obj'
-    FVect = np.array([125, 250, 500, 1000, 2000, 4000])
-    A = np.array([[0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24],
-              [0.08, 0.09, 0.12, 0.16, 0.22, 0.24]]).T
-    # Reflection coefficients
-    R = np.sqrt(1 - A)
-
-    # frequency-dependant scattering coefficients
-    D = np.array([[0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.05, 0.3, 0.7, 0.9, 0.92, 0.94],
-                  [0.01, 0.05, 0.1, 0.2, 0.3, 0.5],
-                  [0.01, 0.05, 0.1, 0.2, 0.3, 0.5]])
-    room = Room(shoebox_file_path, numberOfRays=10, absorptionCoefficients=A,
-                scatteringCoefficients=D, FVect=FVect)
-    
-    
