deefbcc7ab05c10d32c8fdc2de0f4c845070d834
BeneSchier <benedikt.schier@gmail.com>
2023-07-18 22:16:37 +0200

    small change in setup

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/BeneSchier/AcousticZ',
     packages=find_packages(),
+    include_package_data=True,
     package_data={
         'AcousticZ': [
             'data/example_meshes/*.obj'
